47ef2ab2a57a93a1101a767df548e3581dfe8562
tmagnum51-spec <tmagnum51@gmail.com>
2026-05-11 14:55:45 +0200

    mentoring changes

--- app/views/singleBook.php
+++ app/views/singleBook.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($book->getTitle()) ?></title>
-    <link rel="stylesheet" href="/P4-exercice1/public/assets/css/style.css">
+    <link rel="stylesheet" href="public/assets/css/style.css">
 </head>
 <body>
 
